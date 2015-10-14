4ed863e5fada153a4c9d22c931951a6b86fdec94
123 <123@123tekiMacBook-Pro.local>
2015-10-15 00:18:38 +0800

    Initial Commit

---
+++ CardDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardDemo
+//
+//  Created by 123 on 15/10/15.
+//  Copyright (c) 2015年 ivyli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardDemo
+//
+//  Created by 123 on 15/10/15.
+//  Copyright (c) 2015年 ivyli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
